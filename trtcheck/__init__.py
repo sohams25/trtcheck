1c95ce6fca1ee4d8df67cc5968f25b3619bebd29
Soham <sohams.web@gmail.com>
2026-05-22 03:14:51 +0530

    chore: bump version to 1.0.0

--- trtcheck/__init__.py
+++ trtcheck/__init__.py
@@ -3,7 +3,7 @@
 from trtcheck.analyzer import Analyzer, AnalyzerConfig, analyze
 from trtcheck.types import AnalysisReport, CheckCategory, Issue, Severity
 
-__version__ = "0.6.0"
+__version__ = "1.0.0"
 __all__ = [
     "Analyzer",
     "AnalyzerConfig",
